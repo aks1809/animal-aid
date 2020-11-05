5f2e26c934ac635b83ff7d3ad1e43feb565a344d
Arpit <arpitkher22@gmail.com>
2020-11-05 13:09:28 +0530

    add home_page

--- frontend/animal/src/Components/WhatWeDo.js
+++ frontend/animal/src/Components/WhatWeDo.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function WhatWeDo() {
-  return (
-    <div>
-     <h1>What We Do</h1>
-      <h1>What We Do</h1>
-    </div>
+  return ( <
+    div >
+    <
+    h1 > What We Do < /h1> <
+    h1 > What We Do < /h1> <
+    /div>
   );
 }
 
